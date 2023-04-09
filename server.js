fee3929e299d7be3539e307179cbe01a3e5814f0
unknown <o.akindele@omnixbms.com>
2023-04-09 21:57:31 +0100

    added port 3000 as optional port

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connect(DB, {
 })
 .then(() => console.log('DB connection successful'));
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`aamtv app running on port ${port}...`);
 });
